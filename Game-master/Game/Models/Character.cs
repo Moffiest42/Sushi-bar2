faa8ea5f1059b11666fefd72f7149eb707187dcf
Moffiest42 <glebp3434@gmail.com>
2025-05-28 18:33:36 +0400

    full#

--- Game-master/Game/Models/Character.cs
+++ Game-master/Game/Models/Character.cs
@@ -3,26 +3,26 @@
 public class Character
 {
     public int Id { get; set; }
-    [Required(ErrorMessage = "Поле 'Имя' обязательно для заполнения")]
-    [StringLength(60, MinimumLength = 3, ErrorMessage = "Имя должно быть от 3 до 60 символов")]
+    [Required(ErrorMessage = "Некорректный ввод")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "Некорректный ввод")]
     [RegularExpression(@"^[А-Я]+[а-яА-Я\s]*$",
-        ErrorMessage = "Имя должно начинаться с заглавной буквы и содержать только русские буквы")]
-    [Display(Name = "Имя")]
+        ErrorMessage = "Некорректный ввод")]
+    [Display(Name = "Название")]
     public string? Name { get; set; }
 
-    [Required(ErrorMessage = "Поле 'Оружие' обязательно для заполнения")]
-    [StringLength(50, ErrorMessage = "Название оружия не должно превышать 50 символов")]
-    [Display(Name = "Оружие")]
+    [Required(ErrorMessage = "Некорректный ввод")]
+    [StringLength(50, ErrorMessage = "Некорректный ввод")]
+    [Display(Name = "Тип")]
     public string? Weapon { get; set; }
 
-    [Required(ErrorMessage = "Поле 'ID уровня' обязательно для заполнения")]
-    [Display(Name = "ID уровня")]
+    [Required(ErrorMessage = "Некорректный ввод")]
+    [Display(Name = "Количество")]
     public int? LevelId { get; set; }
 
     public Level? Level { get; set; }
 
-    [Required(ErrorMessage = "Поле 'ID игрока' обязательно для заполнения")]
-    [Display(Name = "ID игрока")]
+    [Required(ErrorMessage = "Некорректный ввод")]
+    [Display(Name = "цена за штуку")]
     public int? GamerId { get; set; }
     public Gamer? Gamer { get; set; }
 }
